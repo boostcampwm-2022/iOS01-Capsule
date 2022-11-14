7d907692903402e6f72fdc5c16272b39b1a3d3ab
trumanfromkorea <jangjh971008@naver.com>
2022-11-14 15:25:24 +0900

    [chore] 프로젝트 생성

---
+++ SpaceCapsule/SpaceCapsule/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SpaceCapsule
+//
+//  Created by 장재훈 on 2022/11/14.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
