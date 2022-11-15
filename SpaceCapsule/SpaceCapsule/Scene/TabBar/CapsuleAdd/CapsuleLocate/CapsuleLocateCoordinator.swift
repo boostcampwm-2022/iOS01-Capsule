d8b0d122a01a82769509ddbda65348412d823bb0
trumanfromkorea <jangjh971008@naver.com>
2022-11-15 17:56:20 +0900

    [fix] 누락된 파일 재추가

---
+++ SpaceCapsule/SpaceCapsule/Scene/TabBar/CapsuleAdd/CapsuleLocate/CapsuleLocateCoordinator.swift
@@ -0,0 +1,8 @@
+//
+//  CapsuleLocateCoordinator.swift
+//  SpaceCapsule
+//
+//  Created by 장재훈 on 2022/11/15.
+//
+
+import Foundation
---
+++ SpaceCapsule/SpaceCapsule/Scene/TabBar/CapsuleAdd/CapsuleLocate/CapsuleLocateView.swift
@@ -0,0 +1,8 @@
+//
+//  CapsuleLocateView.swift
+//  SpaceCapsule
+//
+//  Created by 장재훈 on 2022/11/15.
+//
+
+import Foundation
