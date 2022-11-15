5462d7fc2adabe0d4291dc934451fed19593763f
trumanfromkorea <jangjh971008@naver.com>
2022-11-15 18:55:05 +0900

    [chore] Color Asset, Extension 추가 (#1)

---
+++ SpaceCapsule/SpaceCapsule/Extensions/UIColor+.swift
@@ -0,0 +1,20 @@
+//
+//  UIColor+.swift
+//  SpaceCapsule
+//
+//  Created by 장재훈 on 2022/11/15.
+//
+
+import UIKit
+
+extension UIColor {
+    static let themeBackground = UIColor(named: "ThemeBackground")
+    static let themeBlack = UIColor(named: "ThemeBlack")
+    static let themeColor100 = UIColor(named: "ThemeColor100")
+    static let themeColor200 = UIColor(named: "ThemeColor200")
+    static let themeColor300 = UIColor(named: "ThemeColor300")
+    static let themeGray100 = UIColor(named: "ThemeGray100")
+    static let themeGray200 = UIColor(named: "ThemeGray200")
+    static let themeGray300 = UIColor(named: "ThemeGray300")
+    static let themeGray400 = UIColor(named: "ThemeGray400")
+}
