dfcc949b4fa7c723e0efda348354cb0dd08fee0d
Young June Park <pyj9748@naver.com>
2022-11-17 15:03:22 +0900

    [feat] 캡슐 위치 지정화면, 지도에 고정된 커서 추가 (#4)

--- SpaceCapsule/SpaceCapsule/Scene/TabBar/CapsuleAdd/CapsuleLocate/CapsuleLocateView.swift
+++ SpaceCapsule/SpaceCapsule/Scene/TabBar/CapsuleAdd/CapsuleLocate/CapsuleLocateView.swift
@@ -5,4 +5,65 @@
 //  Created by 장재훈 on 2022/11/15.
 //
 
-import Foundation
+
+import UIKit
+import SnapKit
+import MapKit
+
+final class CapsuleLocateView: UIView, BaseView {
+
+    var map: MKMapView = {
+        let mapView = MKMapView()
+        return mapView
+    }()
+    
+    var cursor: UIImageView = {
+        let cursor = UIImageView(image: UIImage(systemName: "circle"))
+        cursor.isUserInteractionEnabled = true
+        cursor.backgroundColor = .red
+        return cursor
+    }()
+    
+    // MARK: - Lifecycle
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+        addSubViews()
+        makeConstraints()
+    }
+
+    @available(*, unavailable)
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    // MARK: - Methods
+
+    func configure() {
+        backgroundColor = .themeBackground
+      
+    }
+
+    
+    func addSubViews() {
+        [map, cursor].forEach {
+            addSubview($0)
+        }
+    }
+
+    func makeConstraints() {
+        
+        map.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+        }
+        
+        cursor.snp.makeConstraints {
+            $0.center.equalToSuperview()
+            $0.height.width.equalTo(20)
+        }
+        
+    }
+}
+
+
