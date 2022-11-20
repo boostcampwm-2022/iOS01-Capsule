255584ec5a12c502657bf2201b79a8c8aac19ff0
Jisu Hong <jisuhongeism@gmail.com>
2022-11-20 18:55:10 +0900

    [feat] CLLocationManager delegate proxy 추가 (#10)

---
+++ SpaceCapsule/SpaceCapsule/Extensions/CLLocationManager+Rx.swift
@@ -0,0 +1,43 @@
+//
+//  CLLocationManager+Rx.swift
+//  SpaceCapsule
+//
+//  Created by jisu on 2022/11/20.
+//
+
+import Foundation
+import RxSwift
+import RxCocoa
+import CoreLocation
+
+class RxCLLocationManagerDelegateProxy: DelegateProxy<CLLocationManager, CLLocationManagerDelegate>, CLLocationManagerDelegate, DelegateProxyType {
+    
+    static func registerKnownImplementations() {
+        self.register { (manager) -> RxCLLocationManagerDelegateProxy in
+               RxCLLocationManagerDelegateProxy(parentObject: manager, delegateProxy: self)
+           }
+    }
+    
+    static func currentDelegate(for object: CLLocationManager) -> CLLocationManagerDelegate? {
+        return object.delegate
+    }
+    
+    static func setCurrentDelegate(_ delegate: CLLocationManagerDelegate?, to object: CLLocationManager) {
+        object.delegate = delegate
+    }
+}
+
+//MARK: location의 변화를 관찰할 observable 추가!
+extension Reactive where Base: CLLocationManager {
+    var delegate: DelegateProxy<CLLocationManager, CLLocationManagerDelegate> {
+        return RxCLLocationManagerDelegateProxy.proxy(for: self.base)
+    }
+    
+    // didUpdateLocations가 일어날 때마다 해당 delegate는 위치를 CLLocation으로 바꾼다
+    var didUpdateLocations: Observable<[CLLocation]> {
+        return delegate.methodInvoked(#selector(CLLocationManagerDelegate.locationManager(_:didUpdateLocations:)))
+            .map { param in
+                return param[1] as? [CLLocation] ?? []
+            }
+    }
+}
