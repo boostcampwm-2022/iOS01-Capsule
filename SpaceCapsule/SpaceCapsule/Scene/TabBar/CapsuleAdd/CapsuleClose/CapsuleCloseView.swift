d2b1be7b55e33e047d2d3419ed82c43344385271
Young June Park <pyj9748@naver.com>
2022-11-21 16:39:51 +0900

    [design] 캡슐 밀봉 화면 UI 구성 (#13)

--- SpaceCapsule/SpaceCapsule/Scene/TabBar/CapsuleAdd/CapsuleClose/CapsuleCloseView.swift
+++ SpaceCapsule/SpaceCapsule/Scene/TabBar/CapsuleAdd/CapsuleClose/CapsuleCloseView.swift
@@ -5,4 +5,143 @@
 //  Created by young june Park on 2022/11/15.
 //
 
-import Foundation
+import UIKit
+import SnapKit
+import AVFoundation
+
+final class CapsuleCloseView: UIView, BaseView {
+    var thumbnailImageView = {
+        let imageView = UIImageView()
+        imageView.layer.cornerRadius = FrameResource.capsuleThumbnailCornerRadius
+        imageView.clipsToBounds = true
+        imageView.image = UIImage.logoWithBG
+        return imageView
+    }()
+    
+    var thumbnailImageContainerView = {
+        let view = UIView()
+        view.layer.shadowOffset = CGSize(width: 4, height: 4)
+        view.layer.shadowRadius = 4
+        view.layer.shadowOpacity = 0.5
+        view.layer.cornerRadius = FrameResource.capsuleThumbnailCornerRadius
+        return view
+    }()
+    
+    var descriptionLabel = {
+        let label = ThemeLabel(text: "xxxx년 x월 x일\nxx시 xx구 에서의\n추억을 담은 캡슐", size: 28, color: .themeGray300)
+        label.numberOfLines = 3
+        label.textAlignment = .center
+        return label
+    }()
+    
+    var closeButton = {
+        let button = UIButton()
+        button.titleLabel?.font = .themeFont(ofSize: 20)
+        button.setTitle("닫기", for: .normal)
+        button.setTitleColor(.white, for: .normal)
+        button.backgroundColor = .themeColor200
+        button.layer.cornerRadius = FrameResource.commonCornerRadius
+        return button
+    }()
+    
+    // MARK: - Lifecycle
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+        addSubViews()
+        makeConstraints()
+        applyUnOpenableEffect()
+    }
+
+    @available(*, unavailable)
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    // MARK: - Methods
+
+    func configure() {
+        backgroundColor = .themeBackground
+    }
+
+    func addSubViews() {
+        [thumbnailImageContainerView, descriptionLabel, closeButton].forEach {
+            addSubview($0)
+        }
+        thumbnailImageContainerView.addSubview(thumbnailImageView)
+    }
+
+    func makeConstraints() {
+        thumbnailImageContainerView.snp.makeConstraints {
+            $0.centerX.equalToSuperview()
+            $0.centerY.equalToSuperview().multipliedBy(0.8)
+            $0.width.equalTo(FrameResource.capsuleThumbnailWidth)
+            $0.height.equalTo(FrameResource.capsuleThumbnailHeigth)
+        }
+        
+        thumbnailImageView.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+        }
+        
+        descriptionLabel.snp.makeConstraints {
+            $0.centerX.equalToSuperview()
+            $0.bottom.equalTo(closeButton.snp.top).offset(-FrameResource.buttonHeight)
+        }
+        
+        closeButton.snp.makeConstraints {
+            $0.leading.equalToSuperview().offset(FrameResource.horizontalPadding)
+            $0.trailing.equalToSuperview().offset(-FrameResource.horizontalPadding)
+            $0.bottom.equalToSuperview().offset(-FrameResource.buttonHeight)
+            $0.height.equalTo(FrameResource.buttonHeight)
+        }
+    }
+    
+    // TODO: Capsule 인자로 받아서 configure하기
+    
+    func applyUnOpenableEffect() {
+        applyBlurEffect()
+        applyLockImage()
+        applyCapsuleDate()
+    }
+    
+    private func applyBlurEffect() {
+        let blurEffect = UIBlurEffect(style: .regular)
+        let blurEffectView = UIVisualEffectView(effect: blurEffect)
+        blurEffectView.layer.cornerRadius = FrameResource.capsuleThumbnailCornerRadius
+        blurEffectView.clipsToBounds = true
+    
+        thumbnailImageView.addSubview(blurEffectView)
+        
+        blurEffectView.snp.makeConstraints {
+            $0.center.equalToSuperview()
+            $0.width.height.equalToSuperview()
+        }
+    }
+    
+    private func applyLockImage() {
+        let lockImageView = UIImageView()
+        lockImageView.image = UIImage(systemName: "lock.fill")
+        lockImageView.tintColor = .themeGray300
+        
+        thumbnailImageView.addSubview(lockImageView)
+        
+        lockImageView.snp.makeConstraints {
+            $0.center.equalToSuperview()
+            $0.width.height.equalTo(50)
+        }
+    }
+    
+    private func applyCapsuleDate() {
+        let dateLabel = ThemeLabel(text: "밀봉시간:xxxx년 x월 x일", size: 18, color: .themeGray300)
+        dateLabel.textAlignment = .center
+        
+        thumbnailImageView.addSubview(dateLabel)
+        
+        dateLabel.snp.makeConstraints {
+            $0.centerX.equalToSuperview()
+            $0.centerY.equalToSuperview().multipliedBy(1.2)
+        }
+    }
+
+}
