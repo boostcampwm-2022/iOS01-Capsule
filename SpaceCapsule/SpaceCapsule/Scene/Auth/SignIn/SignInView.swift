b4a20e7ef5cd47ab27f5e407155dffceb699531f
solbat <alswnd6397@gmail.com>
2022-11-22 14:19:20 +0900

    [design] SignInView 뷰에 로고 추가 (#17)

--- SpaceCapsule/SpaceCapsule/Scene/Auth/SignIn/SignInView.swift
+++ SpaceCapsule/SpaceCapsule/Scene/Auth/SignIn/SignInView.swift
@@ -11,16 +11,18 @@ import AuthenticationServices
 
 final class SignInView: UIView, BaseView {
     // MARK: - UIComponents
-    
+    lazy var logoWithTextView: UIView = {
+        var imageView = UIImageView()
+        imageView.image = UIImage.logoWithText
+        return imageView
+    }()
+        
     lazy var appleSignInButton: ASAuthorizationAppleIDButton = {
         var appleIDButton: ASAuthorizationAppleIDButton = ASAuthorizationAppleIDButton(type: .signIn, style: .black)
-        appleIDButton.layer.cornerRadius = 7
-//        appleIDButton.clipsToBounds = true
         return appleIDButton
     }()
     
     // MARK: - Lifecycles
-    
     override init(frame: CGRect) {
         super.init(frame: frame)
         configure()
@@ -28,27 +30,29 @@ final class SignInView: UIView, BaseView {
         makeConstraints()
     }
     
-    // TODO: 무슨 의미인가?
     @available(*, unavailable)
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
     
     // MARK: - Methods
-    
     func configure() {
-        backgroundColor = .themeBackground
+        backgroundColor = .themeColor100
     }
     
     func addSubViews() {
-        [appleSignInButton].forEach {
-            // MARK: translatesAutoresizingMaskIntoConstraints ??
-//            $0.translatesAutoresizingMaskIntoConstraints = false
+        [logoWithTextView, appleSignInButton].forEach {
             addSubview($0)
         }
     }
     
     func makeConstraints() {
+        logoWithTextView.snp.makeConstraints {
+            $0.width.height.equalTo(FrameResource.logoWithTextSize)
+            $0.centerX.equalToSuperview()
+            $0.centerY.equalToSuperview()
+        }
+        
         appleSignInButton.snp.makeConstraints {
             $0.leading.equalToSuperview().offset(FrameResource.horizontalPadding)
             $0.trailing.equalToSuperview().offset(-FrameResource.horizontalPadding)
