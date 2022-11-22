3c61ed6fd8f31863bf399513c85e2f5cc2d073be
Young June Park <pyj9748@naver.com>
2022-11-22 14:01:00 +0900

    [design] 캡슐 목록 화면 뷰 구성 (#16)

--- SpaceCapsule/SpaceCapsule/Scene/TabBar/CapsuleList/CapsuleListView.swift
+++ SpaceCapsule/SpaceCapsule/Scene/TabBar/CapsuleList/CapsuleListView.swift
@@ -6,10 +6,52 @@
 //
 
 import UIKit
-import RxSwift
+import SnapKit
 
 final class CapsuleListView: UIView, BaseView {
-    func configure() {}
-    func addSubViews() {}
-    func makeConstraints() {}
+    
+    let collectionView = {
+        let collectionView = UICollectionView()
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+        // collectionView.regi 아이템 셀
+        return collectionView
+    }()
+    
+    init() {
+        super.init(frame: .zero)
+        configure()
+        addSubViews()
+        makeConstraints()
+    }
+
+    @available(*, unavailable)
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func configure() {
+        backgroundColor = .themeBackground
+        configureFlowLayout()
+    }
+    
+    func configureFlowLayout() {
+        let collectionViewFlowLayout = UICollectionViewFlowLayout()
+        collectionViewFlowLayout.scrollDirection = .vertical
+        collectionView.collectionViewLayout = collectionViewFlowLayout
+    }
+    
+    func addSubViews() {
+        [collectionView].forEach {
+            addSubview($0)
+        }
+    }
+    
+    func makeConstraints() {
+        collectionView.snp.makeConstraints {
+            $0.top.bottom.equalToSuperview()
+            $0.leading.equalToSuperview().offset(FrameResource.horizontalPadding)
+            $0.trailing.equalToSuperview().offset(-FrameResource.horizontalPadding)
+        }
+    }
+    
 }
