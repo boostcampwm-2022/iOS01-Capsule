2f9cd2a6fe2d7d8c480710277bd4fda9a1aabfc4
Young June Park <pyj9748@naver.com>
2022-11-29 17:44:36 +0900

    [fix] 열 수 있는 이미지 품질 스케일 변환

--- SpaceCapsule/SpaceCapsule/Resources/FrameResource.swift
+++ SpaceCapsule/SpaceCapsule/Resources/FrameResource.swift
@@ -47,7 +47,7 @@ enum FrameResource {
     static let shadowRadius: CGFloat = 4
     static let shadowOpacity: Float = 0.25
     
-    static let openableImageScale: CGFloat = 1.0
+    static let openableImageScale: CGFloat = 2.0
     static let closedImageScale: CGFloat = 0.5
     static let closedIconSize: CGFloat = 50.0
     static let closedDateOffset: CGFloat = 10.0
