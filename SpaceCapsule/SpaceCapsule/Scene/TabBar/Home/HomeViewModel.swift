f5105fc5aaf0208f3ec574c13f46bb59a02ac17a
solbat <alswnd6397@gmail.com>
2022-12-01 15:57:18 +0900

    [Feat] 홈 화면에서 좌우 캡슐 클릭시 선택된 캡슐을 가운데로 이동, 가운데 캡슐 클릭시 애니메이션 주가 (#43)

--- SpaceCapsule/SpaceCapsule/Scene/TabBar/Home/HomeViewModel.swift
+++ SpaceCapsule/SpaceCapsule/Scene/TabBar/Home/HomeViewModel.swift
@@ -5,16 +5,59 @@
 //  Created by young june Park on 2022/11/15.
 //
 
+import CoreLocation
 import Foundation
+import RxCocoa
 import RxSwift
 
 final class HomeViewModel: BaseViewModel {
-    var disposeBag = DisposeBag()
-    var coordinator: HomeCoordinator?
+    weak var coordinator: HomeCoordinator?
+    let disposeBag = DisposeBag()
     
-    init() {
+    var input = Input()
+    var output = Output()
+    
+    struct Input: ViewModelInput {
+        var capsuleCellModels = PublishSubject<[ListCapsuleCellModel]>()
+    }
+    
+    struct Output: ViewModelOutput {
         
     }
     
-    func bind() {}
+    init() {
+        bind()
+    }
+    func fetchCapsuleList() {
+        guard let currentUser = FirebaseAuthManager.shared.currentUser else {
+            return
+        }
+      
+        FirestoreManager.shared.fetchCapsuleList(of: currentUser.uid)
+            .withUnretained(self)
+            .subscribe(
+            onNext: { owner, capsuleList in
+                let capsuleCellModels = capsuleList.map { capsule in
+                    return ListCapsuleCellModel(uuid: capsule.uuid,
+                                                thumbnailImageURL: capsule.images.first,
+                                                address: capsule.simpleAddress,
+                                                closedDate: capsule.closedDate,
+                                                memoryDate: capsule.memoryDate,
+                                                coordinate: CLLocationCoordinate2D(
+                                                    latitude: capsule.geopoint.latitude,
+                                                    longitude: capsule.geopoint.longitude
+                                                )
+                    )
+                }
+                owner.input.capsuleCellModels.onNext(capsuleCellModels)
+            },
+            onError: { error in
+                print(error.localizedDescription)
+            }
+        )
+        .disposed(by: disposeBag)
+    }
+    
+    func bind() {
+    }
 }
