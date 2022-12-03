020d6662aabaf47cfb705fc56c6095ed90a1aabd
Young June Park <pyj9748@naver.com>
2022-12-03 20:32:53 +0900

    [feat] ProfileButton UI 컴포넌트 분리 (#54)

---
+++ SpaceCapsule/SpaceCapsule/Components/Theme/ProfileButton.swift
@@ -0,0 +1,63 @@
+//
+//  ProfileButton.swift
+//  SpaceCapsule
+//
+//  Created by young june Park on 2022/12/03.
+//
+
+import SnapKit
+import UIKit
+
+class ProfileButton: UIView {
+    private let label = ThemeLabel(size: FrameResource.fontSize100, color: .themeGray400)
+    private let icon: UIImageView = {
+        let imageView = UIImageView()
+        imageView.image = .chevronRight
+        imageView.tintColor = .themeGray400
+
+        return imageView
+    }()
+
+    var eventHandler: (() -> Void)?
+
+    convenience init(text: String) {
+        self.init()
+        
+        label.text = text
+        backgroundColor = .themeBackground
+        addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(tapGestureEvent(_:))))
+        addSubViews()
+        makeConstraints()
+    }
+
+    @objc private func tapGestureEvent(_ sender: UITapGestureRecognizer) {
+        eventHandler?()
+    }
+    
+    func setText(_ text: String) {
+        label.text = text
+    }
+
+    private func addSubViews() {
+        [label, icon].forEach {
+            addSubview($0)
+        }
+    }
+
+    private func makeConstraints() {
+        snp.makeConstraints {
+            $0.height.equalTo(FrameResource.profileButtonHeight)
+        }
+
+        label.snp.makeConstraints {
+            $0.centerY.equalToSuperview()
+            $0.leading.equalToSuperview().offset(10)
+        }
+
+        icon.snp.makeConstraints {
+            $0.centerY.equalToSuperview()
+            $0.trailing.equalToSuperview().offset(-10)
+            $0.leading.greaterThanOrEqualTo(label).offset(10)
+        }
+    }
+}
