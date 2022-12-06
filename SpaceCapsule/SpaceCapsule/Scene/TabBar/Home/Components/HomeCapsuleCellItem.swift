a23dd262d1b2f8d85254117c16bbd77e67b4662c
solbat <alswnd6397@gmail.com>
2022-12-06 14:45:44 +0900

    [fix] CellItem으로 이름 변경 및 내부 로직 수정

--- SpaceCapsule/SpaceCapsule/Scene/TabBar/Home/Components/HomeCapsuleCellItem.swift
+++ SpaceCapsule/SpaceCapsule/Scene/TabBar/Home/Components/HomeCapsuleCellItem.swift
@@ -8,7 +8,7 @@
 import UIKit
 import CoreLocation
 
-struct HomeCapsuleCellModel: Hashable, Equatable {
+struct HomeCapsuleCellItem: Hashable, Equatable {
     let uuid: String
     let thumbnailImageURL: String?
     let address: String
@@ -17,8 +17,7 @@ struct HomeCapsuleCellModel: Hashable, Equatable {
     let coordinate: CLLocationCoordinate2D
     
     func isOpenable() -> Bool {
-        let distance = distance()
-        return (distance <= 100.0) ? true : false
+        return LocationManager.shared.isOpenable(capsuleCoordinate: coordinate) ? true : false
     }
     
     static func == (lhs: Self, rhs: Self) -> Bool {
@@ -30,11 +29,6 @@ struct HomeCapsuleCellModel: Hashable, Equatable {
     }
     
     func distance() -> Double {
-        guard let currentCoordinate = LocationManager.shared.coordinate else {
-            return 0.0
-        }
-        let currentLocation = LocationManager.shared.location(currentCoordinate)
-        let capsuleLocation = LocationManager.shared.location(coordinate)
-        return currentLocation.distance(from: capsuleLocation)
+        return LocationManager.shared.distance(capsuleCoordinate: coordinate)
     }
 }
