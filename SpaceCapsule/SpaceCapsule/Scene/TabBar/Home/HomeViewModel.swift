8ff8c5663bac23da0932ea79a1713773c668c4c9
solbat <alswnd6397@gmail.com>
2022-12-07 22:16:23 +0900

    [feat] Capsule 목록에서 특정 조건의 캡슐을 추출하여 CellItem으로 반환 (#84)

--- SpaceCapsule/SpaceCapsule/Scene/TabBar/Home/HomeViewModel.swift
+++ SpaceCapsule/SpaceCapsule/Scene/TabBar/Home/HomeViewModel.swift
@@ -36,17 +36,18 @@ final class HomeViewModel: BaseViewModel {
             .subscribe(
                 onNext: { owner, capsuleList in
                     let capsuleCellItems = capsuleList.map { capsule in
-                        HomeCapsuleCellItem(uuid: capsule.uuid,
-                                            thumbnailImageURL: capsule.images.first,
-                                            address: capsule.simpleAddress,
-                                            closedDate: capsule.closedDate,
-                                            memoryDate: capsule.memoryDate,
-                                            openCount: capsule.openCount,
-                                            coordinate: CLLocationCoordinate2D(
-                                                latitude: capsule.geopoint.latitude,
-                                                longitude: capsule.geopoint.longitude
-                                            ),
-                                            type: .closedOldest
+                        HomeCapsuleCellItem(
+                            uuid: capsule.uuid,
+                            thumbnailImageURL: capsule.images.first,
+                            address: capsule.simpleAddress,
+                            closedDate: capsule.closedDate,
+                            memoryDate: capsule.memoryDate,
+                            openCount: capsule.openCount,
+                            coordinate: CLLocationCoordinate2D(
+                                latitude: capsule.geopoint.latitude,
+                                longitude: capsule.geopoint.longitude
+                            ),
+                            type: .closedOldest
                         )
                     }
                     owner.input.capsules.accept(capsuleList)
@@ -77,6 +78,50 @@ final class HomeViewModel: BaseViewModel {
                     
                 })
             .disposed(by: disposeBag)
+    }
+    func getHomeCapsuleCellItem(capsules: [Capsule], type: FeaturedCapsule) -> HomeCapsuleCellItem? {
+        switch type {
+        case .closedOldest:
+            let capsule = getClosedOldest(capsules: capsules)
+            return makeHomeCapsuleCellItem(capsule: capsule, type: type)
+        case .closedNewest:
+            let capsule = getClosedNewest(capsules: capsules)
+            return makeHomeCapsuleCellItem(capsule: capsule, type: type)
+        case .memoryOldest:
+            let capsule = getMemoryOldest(capsules: capsules)
+            return makeHomeCapsuleCellItem(capsule: capsule, type: type)
+        case .memoryNewest:
+            let capsule = getMemoryNewest(capsules: capsules)
+            return makeHomeCapsuleCellItem(capsule: capsule, type: type)
+        case .nearest:
+            let capsule = getNearest(capsules: capsules)
+            return makeHomeCapsuleCellItem(capsule: capsule, type: type)
+        case .farthest:
+            let capsule = getFarthest(capsules: capsules)
+            return makeHomeCapsuleCellItem(capsule: capsule, type: type)
+        case .leastOpened:
+            let capsule = getLeastOpened(capsules: capsules)
+            return makeHomeCapsuleCellItem(capsule: capsule, type: type)
+        }
+    }
+    
+    func makeHomeCapsuleCellItem(capsule: Capsule?, type: FeaturedCapsule) -> HomeCapsuleCellItem? {
+        guard let capsule else {
+            return nil
+        }
+        return HomeCapsuleCellItem(
+            uuid: capsule.uuid,
+            thumbnailImageURL: capsule.images.first,
+            address: capsule.simpleAddress,
+            closedDate: capsule.closedDate,
+            memoryDate: capsule.memoryDate,
+            openCount: capsule.openCount,
+            coordinate: CLLocationCoordinate2D(
+                latitude: capsule.geopoint.latitude,
+                longitude: capsule.geopoint.longitude
+            ),
+            type: type
+        )
     }
     
     func getClosedOldest(capsules: [Capsule]) -> Capsule? {
