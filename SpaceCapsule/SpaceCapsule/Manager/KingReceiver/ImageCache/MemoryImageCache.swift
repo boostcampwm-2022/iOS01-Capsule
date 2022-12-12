04b3960d4651daee9509c57c1c2e39b81396a535
trumanfromkorea <jangjh971008@naver.com>
2022-12-12 14:16:17 +0900

    [fix] 메모리 캐시 용량 지정방식 변경

--- SpaceCapsule/SpaceCapsule/Manager/KingReceiver/ImageCache/MemoryImageCache.swift
+++ SpaceCapsule/SpaceCapsule/Manager/KingReceiver/ImageCache/MemoryImageCache.swift
@@ -9,12 +9,11 @@ import Foundation
 
 /// 메모리 -> `NSCache` 에 캐싱
 final class MemoryImageCache: ImageCache {
-    private let cache: NSCache = {
-        let cache = NSCache<NSString, NSData>()
-        cache.totalCostLimit = 52428800
+    private let cache = NSCache<NSString, NSData>()
 
-        return cache
-    }()
+    init(cacheLimit: Int = 52428800) {
+        cache.totalCostLimit = cacheLimit
+    }
 
     func fetch(with url: URL, completion: @escaping (Data?) -> Void) {
         if let data = cache[url] {
