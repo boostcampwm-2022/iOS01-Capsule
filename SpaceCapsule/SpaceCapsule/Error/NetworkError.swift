4d934f9a391f112e07bbae8cea31463db3e7eef9
Young June Park <pyj9748@naver.com>
2022-12-13 09:56:23 +0900

    [refactor] Error 생성

--- SpaceCapsule/SpaceCapsule/Error/NetworkError.swift
+++ SpaceCapsule/SpaceCapsule/Error/NetworkError.swift
@@ -10,4 +10,22 @@ import Foundation
 enum NetworkError: Error {
     case failedConvertingStringToUrl
     case decodingError
+    case refreshTokenError
+    case revokeTokenError
+    case clientSecretError
+
+    var errorDescription: String {
+        switch self {
+        case .failedConvertingStringToUrl:
+            return "failedConvertingStringToUrl"
+        case .decodingError:
+            return "decodingError"
+        case .refreshTokenError:
+            return "refreshTokenError"
+        case .revokeTokenError:
+            return "revokeTokenError"
+        case .clientSecretError:
+            return "clientSecretError"
+        }
+    }
 }
