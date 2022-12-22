c19e45080c995c7f6536a130426d79c37ee15596
solbat <alswnd6397@gmail.com>
2022-12-22 19:12:32 +0900

    [feat] MyCollectionView : reloadData가 종료되는 시점을 파악하도록 구현 (#123)

---
+++ SpaceCapsule/SpaceCapsule/Scene/TabBar/Home/MyCollectionView.swift
@@ -0,0 +1,24 @@
+//
+//  MyCollectionView.swift
+//  SpaceCapsule
+//
+//  Created by 김민중 on 2022/12/22.
+//
+
+import UIKit
+
+class MyCollectionView: UICollectionView {
+
+    private var reloadDataCompletionBlock: (() -> Void)?
+    
+    func reloadDataWithCompletion(_ complete: @escaping () -> Void) {
+        reloadDataCompletionBlock = complete
+        super.reloadData()
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        reloadDataCompletionBlock?()
+        reloadDataCompletionBlock = nil
+    }
+}
